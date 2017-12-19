19a6c8b04cc027a481c7ad60cfce7df810bd4023
cesare moschini <Ze@MBP-di-cesare.lan>
2017-12-19 22:42:37 +0100

    update

--- test.js
+++ test.js
@@ -11,18 +11,6 @@ var emptyarray = [];
 test('delete obj',function(assert){
   assert.isEqual( rofa(arrayofobjects, { a : 'a' , b : 'b'}), true);
   assert.isEqual( rofa(arrayofobjects, { c : 'c' , d : 'd'}), true);
-  assert.end();
-});
-
-test('obj not found',function(assert){
-  assert.isEqual( rofa(arrayofobjects,{}),false);
-  assert.isEqual( rofa(arrayofobjects,{x : 'x'}),false);
-  assert.end();
-});
-
-test('null values',function(assert){
-  assert.isEqual( rofa(arrayofobjects, null),false);
-  assert.isEqual( rofa(arrayofobjects,{ x : null }),false);
-  assert.isEqual( rofa(null, null),false);
+  assert.isEqual(arrayofobjects.length,0);
   assert.end();
 });
